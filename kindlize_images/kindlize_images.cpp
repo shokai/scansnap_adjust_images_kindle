d950fc24c8aa1bb7e1a7b6168ecf45fd9c8a0fd4
Sho Hashimoto <hashimoto@shokai.org>
2010-09-14 16:54:23 +0900

    fix height/width scale

--- kindlize_images/kindlize_images.cpp
+++ kindlize_images/kindlize_images.cpp
@@ -15,22 +15,26 @@ IplImage *adjust_image(IplImage *img, program_options::variables_map argmap){
   IplImage *img_gray = cvCreateImage(cvSize(w,h), IPL_DEPTH_8U, 1);
   cvCvtColor(img, img_gray, CV_BGR2GRAY);
 
+  // 2値化
   IplImage *img_bin = cvCreateImage(cvSize(w,h), IPL_DEPTH_8U, 1);
   cvThreshold(img_gray, img_bin, argmap["threshold"].as<int>(), 255, CV_THRESH_BINARY);
 
+  // 文字を太らせる
+  cvErode(img_bin, img_bin, NULL, 1);
+
+  // リサイズ
   int width = argmap["width"].as<int>();
   int height = argmap["height"].as<int>();
-  
   IplImage *img_resized;
   double scale = ((double)h)/w;
-  if(((double)height)/width < scale){
-    img_resized = cvCreateImage(cvSize(width, (int)(width*scale)), IPL_DEPTH_8U, 1);
+  if(((double)height)/width < scale){ // 縦長
+    img_resized = cvCreateImage(cvSize((int)(height/scale), height), IPL_DEPTH_8U, 1);
   }
-  else{
-    img_resized = cvCreateImage(cvSize((int)(height*scale), height), IPL_DEPTH_8U, 1);
+  else{ // 横長
+    img_resized = cvCreateImage(cvSize(width, (int)(scale*width)), IPL_DEPTH_8U, 1);
   }
   cvResize(img_bin, img_resized, CV_INTER_LINEAR);
-
+  
   //IplImage *img_frame = cvCreateImage(cvSize(WIDTH, HEIGHT), IPL_DEPTH_8U, 1);
   
   return img_resized;
